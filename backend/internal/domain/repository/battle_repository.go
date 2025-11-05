c1d61d1a4469e2047d1415b95bbaa90cc60137bf
guu <Yu0gura1624@outlook.jp>
2025-11-06 01:13:18 +0900

    バトルのドメイン、リポジトリ

---
+++ backend/internal/domain/repository/battle_repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/ABfry/album-battler/backend/internal/domain/entity"
+	"github.com/google/uuid"
+)
+
+type BattleRepository interface {
+	Create(ctx context.Context, battle *entity.Battle) error
+	FindByID(ctx context.Context, id string) (*entity.Battle, error)
+	FindByRoomID(ctx context.Context, roomID int) (*entity.Battle, error)
+	FindByUserID(ctx context.Context, user uuid.UUID) (*entity.Battle, error)
+	Save(ctx context.Context, battle *entity.Battle) error
+}
