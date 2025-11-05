b72d91fd1248998c81c662cf35e798e68d9b2410
guu <Yu0gura1624@outlook.jp>
2025-11-06 00:52:30 +0900

    部屋修正

--- backend/internal/domain/entity/room.go
+++ backend/internal/domain/entity/room.go
@@ -18,43 +18,35 @@ const (
 )
 
 type Room struct {
-	ID uuid.UUID
-	RoomID int
+	ID         uuid.UUID
+	RoomID     int // 1~9999
 	HostUserID uuid.UUID
-	CreatedAt time.Time
-	ExpiredAt time.Time
-	Status RoomStatus
+	CreatedAt  time.Time
+	ExpiredAt  time.Time
+	Status     RoomStatus
 }
 
-func NewRoom(roomID int, hostUserID uuid.UUID, expiredAt time.Time) *Room {
-	return &Room{
-		ID: uuid.New(),
-		RoomID: roomID,
-		HostUserID: hostUserID,
-		CreatedAt: time.Now(),
-		ExpiredAt: expiredAt,
-		Status: WaitJoin,
-	}
-}
-
-func (r *Room) CreateRoom(roomID int, hostUserID uuid.UUID, expiredAt time.Time) error {
+func NewRoom(roomID int, hostUserID uuid.UUID, expiredAt time.Time) (*Room, error) {
 	if roomID <= 0 {
-		return errors.New("roomID is invalid")
+		return nil, errors.New("roomID is invalid")
 	}
 	if hostUserID == uuid.Nil {
-		return errors.New("hostUserID is required")
+		return nil, errors.New("hostUserID is required")
 	}
 	if expiredAt.IsZero() {
-		return errors.New("expiredAt is required")
+		return nil, errors.New("expiredAt is required")
 	} else if expiredAt.Before(time.Now()) {
-		return errors.New("expiredAt must be in the future")
+		return nil, errors.New("expiredAt must be in the future")
 	}
 
-	r.RoomID = roomID
-	r.HostUserID = hostUserID
-	r.ExpiredAt = expiredAt
-
-	return nil
+	return &Room{
+		ID:         uuid.New(),
+		RoomID:     roomID,
+		HostUserID: hostUserID,
+		CreatedAt:  time.Now(),
+		ExpiredAt:  expiredAt,
+		Status:     WaitJoin,
+	}, nil
 }
 
 func (r *Room) ChangeStatus(status RoomStatus) {
