8ac980e729ce51ea1ffdfd94f67594e05ef876b6
guu <Yu0gura1624@outlook.jp>
2025-11-06 00:46:05 +0900

    Context追加

--- backend/internal/domain/repository/room_repository.go
+++ backend/internal/domain/repository/room_repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "github.com/ABfry/album-battler/backend/internal/domain/entity"
+import (
+	"context"
+
+	"github.com/ABfry/album-battler/backend/internal/domain/entity"
+)
 
 type RoomRepository interface {
-	Create(room *entity.Room) error
-	FindByID(id string) (*entity.Room, error)
-	FindByRoomID(roomID int) (*entity.Room, error)
-	Save(room *entity.Room) error
+	Create(ctx context.Context, room *entity.Room) error
+	FindByID(ctx context.Context, id string) (*entity.Room, error)
+	FindByRoomID(ctx context.Context, roomID int) (*entity.Room, error)
+	Save(ctx context.Context, room *entity.Room) error
 }
