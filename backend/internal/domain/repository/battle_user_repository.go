e0ba01ebf7999090950d15cd1056556568fd1abd
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-11-09 23:29:53 +0900

    refactor(repository): BattleUserRepositoryのインターフェースを変更

--- backend/internal/domain/repository/battle_user_repository.go
+++ backend/internal/domain/repository/battle_user_repository.go
@@ -3,13 +3,9 @@ package repository
 import (
 	"context"
 
-	"github.com/ABfry/album-battler/backend/internal/domain/entity"
 	"github.com/google/uuid"
 )
 
 type BattleUserRepository interface {
-	FindByID(ctx context.Context, id uuid.UUID) (*entity.BattleUser, error)
-	FindByUserID(ctx context.Context, userID uuid.UUID) (*entity.BattleUser, error)
-	FindByBattleID(ctx context.Context, battleID uuid.UUID) (*entity.BattleUser, error)
-	Save(ctx context.Context, battleUser *entity.BattleUser) error
+	Save(ctx context.Context, battleID, userID uuid.UUID) error
 }
