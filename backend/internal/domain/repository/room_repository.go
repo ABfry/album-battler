d9473b4cce51a8d32c9458999fa5967b76e618ee
guu <Yu0gura1624@outlook.jp>
2025-11-11 01:46:02 +0900

    refactor(room): FindByIDメソッドのID型をstringからuuid.UUIDに変更

--- backend/internal/domain/repository/room_repository.go
+++ backend/internal/domain/repository/room_repository.go
@@ -4,10 +4,11 @@ import (
 	"context"
 
 	"github.com/ABfry/album-battler/backend/internal/domain/entity"
+	"github.com/google/uuid"
 )
 
 type RoomRepository interface {
-	FindByID(ctx context.Context, id string) (*entity.Room, error)
+	FindByID(ctx context.Context, id uuid.UUID) (*entity.Room, error)
 	FindByRoomNumber(ctx context.Context, roomNumber int) (*entity.Room, error)
 	Save(ctx context.Context, room *entity.Room) error
 }
