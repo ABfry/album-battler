a0053b2a5d109da794dea13e21d63e7bdee79294
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-11-11 00:42:47 +0900

    feat(room): ルーム機能に全ルーム取得機能を追加

--- backend/internal/domain/repository/room_repository.go
+++ backend/internal/domain/repository/room_repository.go
@@ -11,5 +11,6 @@ type RoomRepository interface {
 	FindByID(ctx context.Context, id uuid.UUID) (*entity.Room, error)
 	FindByRoomNumber(ctx context.Context, roomNumber int) (*entity.Room, error)
 	FindByHostUserID(ctx context.Context, hostUserID uuid.UUID) (*entity.Room, error)
+	FindAll(ctx context.Context) ([]*entity.Room, error)
 	Save(ctx context.Context, room *entity.Room) error
 }
