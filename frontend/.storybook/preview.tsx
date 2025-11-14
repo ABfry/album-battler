aad524a9597c33448619f2e008dc61c5eef78596
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-11-14 21:57:45 +0900

    feat(storybook): Storybookの設定ファイルとボタンコンポーネントのストーリーを追加

---
+++ frontend/.storybook/preview.tsx
@@ -0,0 +1,22 @@
+import type { Preview } from "@storybook/react";
+import "../app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
