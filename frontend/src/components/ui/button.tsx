7164cd54f4763fd420fe6bc8055b4c821a8bd4c9
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-11-23 13:46:13 +0900

    style(button): cssfix

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const defaultButtonStyles =
 
 const buttonVariants = cva(
   // ここは共通のレイアウト・状態系クラス（見た目ではなく挙動寄り）
-  "inline-flex select-none` items-center justify-center gap-2 whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex select-none items-center justify-center gap-2 whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
